e28d59391425fd87280ea7a494424d2207f74180
amrnayer <amrnayer1@gmail.com>
2018-11-28 19:26:10 +0200

    Code File

--- Maxi.ino
+++ Maxi.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal.h> // includes the LiquidCrystal Library 
 
 LiquidCrystal lcd(3, 2, 4, 5, 6, 7);//LCD object
+  
 byte heart[8] = {
   B00000,
   B00000,
@@ -11,25 +12,26 @@ byte heart[8] = {
   B00100,
   B00000
 };//heart on screen
-String Name="";
-String voice="";
-String Mood="";
+
+String Name=""  ;   //Name that user enter
+String voice="";    //User interact Hi/Bye
+String Mood="";     //Mood that 
 void setup() {
-  Serial.begin(9600);
-  lcd.begin(16,2);
+    Serial.begin(9600); //bluetooth module begin
+    lcd.begin(16,2);    //LCD begin
   
-   lcd.createChar(0, heart);
- lcd.clear();
-lcd.print("Hello");
+    lcd.createChar(0, heart);
+    lcd.clear();
+    lcd.print("Hello");
  
 }
 
 void loop() {
 
-while(  Serial.available())
+while(  Serial.available())     //take input from blutooth module through voice processing app
 {
   char c=(char)Serial.read();
-  voice+=c;
+  voice+=c;                     // put input in Voice variable
   
 }delay(2000);
   Serial.print(voice);
@@ -38,86 +40,61 @@ while(  Serial.available())
 
 if(voice.length()>0){
   Serial.println(voice);
-
-
-
-  
-
 if(voice=="hi")
-{lcd.clear();
-lcd.print("Hello");
-
-
-    lcd.clear();
-  lcd.print("What is ur name?");
-  delay(8000);
-  while(  Serial.available())
 {
+    lcd.clear();
+    lcd.print("Hello");
+    lcd.clear();
+    lcd.print("What is ur name?");
+    delay(8000);
+  while(  Serial.available())
+  {
   char a=(char)Serial.read();
   Name+=a;
   Serial.print(Name);
   
-}
-
-  lcd.clear();
-  lcd.print("hi");
-   lcd.setCursor(3,0);
-   lcd.print(Name);
-   lcd.setCursor(2,1);
-   lcd.print("I'm Maxi");
+  }
+    lcd.clear();
+    lcd.print("hi"); 
+    lcd.setCursor(3,0);
+    lcd.print(Name);
+    lcd.setCursor(2,1);
+    lcd.print("I'm Maxi");
     lcd.print(char(0));
     lcd.print(char(0));
-     lcd.print(char(0));
-     delay(2500);
-     lcd.clear();
-
-     lcd.print("are you ");
-     lcd.setCursor(2,1);
-     lcd.print("Felling good?");
-       delay(8000);
+    lcd.print(char(0));
+    delay(2500);
+    lcd.clear();
+    lcd.print("are you ");
+    lcd.setCursor(2,1);
+    lcd.print("Felling good?");
+    delay(8000);
   while(  Serial.available())
-{
+  {
   char x=(char)Serial.read();
-  Mood+=x;
-  
-}
-
+  Mood+=x; 
+  }
   if(Mood=="yes")
   {
     lcd.clear();
-    lcd.print("el7amdellah");
-  
-    
-    }
+    lcd.print("el7amdellah");   
+  }
     else if(Mood="no")
     {
-      lcd.clear();
-    lcd.print("m3lish");
- 
-    
-     
-      }
-   
-   
+    lcd.clear();
+    lcd.print("m3lish");     
+    }   
 }
-
 else if(voice=="off")
 {
   lcd.clear();
   lcd.print("Bye");
   lcd.setCursor(2,1);
-  lcd.print("see you Later");
-  
-  
-
-  
+  lcd.print("see you Later"); 
 }
-
 Name="";
 Mood="";
 voice="";
-
 }
-
 }
 
